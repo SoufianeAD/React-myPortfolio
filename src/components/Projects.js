3816d78921460c4364a9e398e65c865bc9d18dcf
Soufiane Ammy Driss <=>
2021-03-30 00:07:14 +0200

    update cvs! removing the bar on the portfolio section!

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -40,17 +40,7 @@ class Projects extends  React.Component {
                         <h2>Portfolio</h2>
                     </div>
 
-                    <div className="row" data-aos="fade-up">
-                        <div className="col-lg-12 d-flex justify-content-center">
-                            <ul id="portfolio-flters">
-                                <li data-filter="*" className="filter-active">All</li>
-                                <li data-filter=".filter-app">Web</li>
-                                <li data-filter=".filter-card">Mobile</li>
-                                <li data-filter=".filter-web">Desktop</li>
-                                <li data-filter=".filter-web">Data Science</li>
-                            </ul>
-                        </div>
-                    </div>
+
 
                     <div className="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
                         {this.renderList()}
